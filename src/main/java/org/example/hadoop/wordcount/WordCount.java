5deea33b5b1b08b84deaf6c0282cf4d6ae2528e5
Pramod <pramodbeheradkl@gamil.com>
2026-02-14 19:31:23 +0530

    Final solution-2 to 9

--- src/main/java/org/example/hadoop/wordcount/WordCount.java
+++ src/main/java/org/example/hadoop/wordcount/WordCount.java
@@ -2,10 +2,10 @@ package org.example.hadoop.wordcount;
 
 import java.io.IOException;
 import java.util.StringTokenizer;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -16,14 +16,17 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class WordCount {
 
     public static class TokenizerMapper
-            extends Mapper<Object, Text, Text, IntWritable>{
+            extends Mapper<LongWritable, Text, Text, IntWritable>{
 
         private final static IntWritable one = new IntWritable(1);
         private final Text word = new Text();
 
-        public void map(Object key, Text value, Context context
-        ) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
+        @Override
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+            String line = value.toString().replaceAll("[^a-zA-Z0-9\\s']", " ").toLowerCase();
+
+            StringTokenizer itr = new StringTokenizer(line);
             while (itr.hasMoreTokens()) {
                 word.set(itr.nextToken());
                 context.write(word, one);
@@ -35,9 +38,8 @@ public class WordCount {
             extends Reducer<Text,IntWritable,Text,IntWritable> {
         private final IntWritable result = new IntWritable();
 
-        public void reduce(Text key, Iterable<IntWritable> values,
-                           Context context
-        ) throws IOException, InterruptedException {
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             int sum = 0;
             for (IntWritable val : values) {
                 sum += val.get();
@@ -49,15 +51,29 @@ public class WordCount {
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
+
+        long startTime = System.currentTimeMillis();
+
         Job job = Job.getInstance(conf, "word count");
         job.setJarByClass(WordCount.class);
         job.setMapperClass(TokenizerMapper.class);
         job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
+
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
+
+
+        job.getConfiguration().setLong("mapreduce.input.fileinputformat.split.maxsize", 67108864); // 64MB
+
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+        boolean success = job.waitForCompletion(true);
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Total Execution Time: " + (endTime - startTime) + "ms");
+
+        System.exit(success ? 0 : 1);
     }
 }
